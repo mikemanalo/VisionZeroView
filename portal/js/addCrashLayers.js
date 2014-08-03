12964c223b9679328aba484d9501e455d31b8361
hadekoya <hadekoya@gmail.com>
2014-08-03 14:23:18 -0400

    added show loading spinner to all dynamic map services

--- portal/js/addCrashLayers.js
+++ portal/js/addCrashLayers.js
@@ -6,56 +6,25 @@ function addCrashLayers() {
 		id : "fatality_monthly_all_Layer"
 
 	});
-	
-	
-	/*************** test July 24 **********************************************************/
-	/*fatality_monthly_all_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://dotdevgisiis01/ArcGIS/rest/services/GISAPP_VISION_ZERO/monthlyTotalFatalities_Seth_2009_2014/MapServer", {
-		id : "fatality_monthly_all_Layer"
-
-	});*/
-	
-	
-	map.addLayer(fatality_monthly_all_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	
-	fatality_monthly_all_Layer.setVisibility(false);
-	
-	
+	AddLayer(fatality_monthly_all_Layer);
 
 	fatality_monthly_ped_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/pedFatalities_monthly/MapServer", {
 		id : "fatality_monthly_ped_Layer"
 
 	});
-	map.addLayer(fatality_monthly_ped_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_monthly_ped_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(fatality_monthly_ped_Layer);
 	
 	fatality_monthly_bike_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/bikeFatalities_monthly/MapServer", {
 		id : "fatality_monthly_bike_Layer"
 
 	});
-	map.addLayer(fatality_monthly_bike_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_monthly_bike_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(fatality_monthly_bike_Layer);
 	
 	fatality_monthly_motor_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/motorFatalities_monthly/MapServer", {
 		id : "fatality_monthly_motor_Layer"
 
 	});
-	map.addLayer(fatality_monthly_motor_Layer);
-	console.log("added");
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_monthly_motor_Layer.setVisibility(false);
+	AddLayer(fatality_monthly_motor_Layer);
 	
 	
 	/******************************************** YEARLY FATALITY ************************************************************/
@@ -66,45 +35,25 @@ function addCrashLayers() {
 		
 
 	});
-	map.addLayer(fatality_yearly_all_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_yearly_all_Layer.setVisibility(false);
+	AddLayer(fatality_yearly_all_Layer);
 	
-	
-
 	fatality_yearly_ped_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/pedFatalities_yearly/MapServer", {
 		id : "fatality_yearly_ped_Layer"
 
 	});
-	map.addLayer(fatality_yearly_ped_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_yearly_ped_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(fatality_yearly_ped_Layer);
 	
 	fatality_yearly_bike_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/bikeFatalities_yearly/MapServer", {
 		id : "fatality_yearly_bike_Layer"
 
 	});
-	map.addLayer(fatality_yearly_bike_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_yearly_bike_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(fatality_yearly_bike_Layer);
 	
 	fatality_yearly_motor_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/motorFatalities_yearly/MapServer", {
 		id : "fatality_yearly_motor_Layer"
 
 	});
-	map.addLayer(fatality_yearly_motor_Layer);
-	//var testArray = [];
-	//testArray[0] = 0;
-	fatality_yearly_motor_Layer.setVisibility(false);
+	AddLayer(fatality_yearly_motor_Layer);
 	
 	
 	
@@ -117,53 +66,26 @@ function addCrashLayers() {
 		
 
 	});
-	
-	/************ test July 24 ******************************************************/
-	/*injury_monthly_all_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://dotdevgisiis01/ArcGIS/rest/services/GISAPP_VISION_ZERO/monthlyInjuries_2009_2014_June13/MapServer", {
-		id : "injury_monthly_all_Layer"
-		
-
-	});*/
-	
-	map.addLayer(injury_monthly_all_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_monthly_all_Layer.setVisibility(false);
-
-
-
+	AddLayer(injury_monthly_all_Layer);
+    
 	injury_monthly_ped_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/pedFatalities_and_Injuries_monthly/MapServer", {
 		id : "injury_monthly_ped_Layer"
 
 	});
-	map.addLayer(injury_monthly_ped_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_monthly_ped_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(injury_monthly_ped_Layer);
+    
 	injury_monthly_bike_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/bikeFatalities_and_Injuries_monthly/MapServer", {
 		id : "injury_monthly_bike_Layer"
 
 	});
-	map.addLayer(injury_monthly_bike_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_monthly_bike_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(injury_monthly_bike_Layer);
+    	
 	injury_monthly_motor_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/motorFatalities_and_Injuries_monthly/MapServer", {
 		id : "injury_monthly_motor_Layer"
 
 	});
-	map.addLayer(injury_monthly_motor_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_monthly_motor_Layer.setVisibility(false);
-	
-	
+	AddLayer(injury_monthly_motor_Layer);
+
 	
 	/***************************************************** INJURY YEARLY ***************************************************************************************/
 	
@@ -174,50 +96,45 @@ function addCrashLayers() {
 		,opacity:.8
 
 	});
-	map.addLayer(injury_yearly_all_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_yearly_all_Layer.setVisibility(false);
-
-
+	AddLayer(injury_yearly_all_Layer);
 
 	injury_yearly_ped_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/pedInjury_yearly/MapServer", {
 		id : "injury_yearly_ped_Layer"
 		,opacity:.8
 
 	});
-	map.addLayer(injury_yearly_ped_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_yearly_ped_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(injury_yearly_ped_Layer);
+		
 	injury_yearly_bike_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/bikeInjury_yearly/MapServer", {
 		id : "injury_yearly_bike_Layer"
 		,opacity:.8
 
 	});
-	map.addLayer(injury_yearly_bike_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_yearly_bike_Layer.setVisibility(false);
-	
-	
-	
+	AddLayer(injury_yearly_bike_Layer);
+
 	injury_yearly_motor_Layer = new esri.layers.ArcGISDynamicMapServiceLayer("http://" + arcgisserver + "/arcgis/rest/services/VISION_ZERO/motorInjury_yearly/MapServer", {
 		id : "injury_yearly_motor_Layer"
 		,opacity:.8
 
 	});
-	map.addLayer(injury_yearly_motor_Layer);
-	//var allInjuryArray = [];
-	//allInjuryArray[0] = 0;
-	injury_yearly_motor_Layer.setVisibility(false);	
 	
+	AddLayer(injury_yearly_motor_Layer);
 
 
 } // end addLayers
+
+function AddLayer(layerName) {
+    layerName.on("update-start", function () {
+        showLoading();
+    });
+
+    layerName.on("update-end", function () {
+        hideLoading();
+    });
+
+    map.addLayer(layerName);
+    layerName.setVisibility(false);
+}
 
 
 
