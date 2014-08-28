75fb3aefdb87d39e9b6e77c7f8ab6c5fcd7c73b9
hadekoya <hadekoya@gmail.com>
2014-08-28 11:18:00 -0400

    added basemap url to config file, added CORS detection to false, added domain name detection for mapservice

--- portal/js/map_config.js
+++ portal/js/map_config.js
@@ -2,4 +2,9 @@
 
 
         //var arcgisserver = "dotqagisiis03:6080"; // http://dotqavgisweb:6080/
-        var arcgisserver = "a841-dotqaweb01.nyc.gov";
+    var arcgisserver = "a841-dotqaweb01.nyc.gov";
+
+    var domainName = window.location.host;
+    var mapServiceDomain = window.location.host + ":6080";
+    //var arcgisserver = mapServiceDomain;//"54.164.42.242:6080";
+    var basemapURL = "http://a841-dotweb01.nyc.gov/arcgis/rest/services/GAZETTEER/NYCDOT_Basemap_2013_Grey/MapServer";
