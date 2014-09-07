1f9ff5b2e3e3770a58760be59c8ef3f4823d9552
Hassan Adekoya <hadekoya@gmail.com>
2014-09-07 18:18:42 -0400

    refactored config file to detect QA vs Prod environment variable

--- portal/js/map_config.js
+++ portal/js/map_config.js
@@ -1,12 +1,18 @@
 
+	var environment = "QA"; //QA or PROD
 
-
+	var arcgisserver = "a841-dotqaweb01.nyc.gov";
+	var basemapURL = "http://a841-dotweb01.nyc.gov/arcgis/rest/services/GAZETTEER/NYCDOT_Basemap_2013_Grey/MapServer";
+	
+	if (environment == "PROD"){
+		 var domainName= window.location.host;
+		 var mapServiceDomain= window.location.host+":6080";
+		 arcgisserver = mapServiceDomain;//"54.164.42.242:6080";
+	} 
         
         //var arcgisserver = "dotqagisiis03:6080"; // http://dotqavgisweb:6080/
         //var arcgisserver = "http://nycdotvzlb-478704647.us-east-1.elb.amazonaws.com:6080";
 		
-		  var domainName= window.location.host;
-		  var mapServiceDomain= window.location.host+":6080";
-		 // var arcgisserver = mapServiceDomain;//"54.164.42.242:6080";
-		 var arcgisserver = "a841-dotqaweb01.nyc.gov";
-		  var basemapURL = "http://a841-dotweb01.nyc.gov/arcgis/rest/services/GAZETTEER/NYCDOT_Basemap_2013_Grey/MapServer";
+		
+		 
+		  
